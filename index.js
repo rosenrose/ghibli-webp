6036bc5bf1788eb36c96ba1ed38709194dba07a6
rosenrose <42435748+rosenrose@users.noreply.github.com>
2022-02-21 23:40:11 +0900

    d

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ const express = require("express");
 
 const app = express();
 app.get("/", (req, res) => res.send("<h1>Test</h1>"));
-app.listen(80);
+
+const httpServer = http.createServer(app);
+httpServer.listen(80);
