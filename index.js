3abcec4fe07fcae81bf7d3e2e66ad5e8c8e60413
rosenrose <42435748+rosenrose@users.noreply.github.com>
2022-02-22 19:01:52 +0900

    cors

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const http = require("http");
 const express = require("express");
 const axios = require("axios").default;
 const { createFFmpeg, fetchFile } = require("@ffmpeg/ffmpeg");
-const ffmpeg = createFFmpeg({ log: true });
-ffmpeg.load();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-app.get("/", (req, res) => res.send(`<h1>Test port: ${PORT}</h1>`));
+app.get("/", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.send(`<h1>Test port: ${PORT}</h1>`);
+});
 
 const httpServer = http.createServer(app);
 httpServer.listen(PORT);
@@ -20,46 +21,62 @@ const io = require("socket.io")(httpServer, {
   },
 });
 
-io.on("connection", (socket) => {
-  socket.on("webp", async (done) => {
-    const time = Date.now().toString();
-    const rand = getRandomInt(100, 1000);
-    const outputName = "a.webp";
-    const downloadPromises = [];
+io.on("connection", async (socket) => {
+  let queue = false;
+  const ffmpeg = createFFmpeg({ log: true });
 
-    ffmpeg.FS("mkdir", time);
-    for (let i = 0; i < 18; i++) {
-      let name = `${(rand + i).toString().padStart(5, "0")}.jpg`;
-
-      downloadPromises.push(
-        new Promise((resolve) => {
-          axios(encodeURI(`https://d2wwh0934dzo2k.cloudfront.net/ghibli/07 붉은 돼지 (1992)/${name}`), {
-            responseType: "arraybuffer",
-          }).then((response) => {
-            ffmpeg.FS("writeFile", `${time}/${name}`, response.data);
-            resolve();
-          });
-        })
-      );
+  ffmpeg.load().then(() => {
+    if (queue) {
+      runWebp(ffmpeg);
     }
-    await Promise.all(downloadPromises);
-    await ffmpeg.run(
-      "-framerate",
-      "12",
-      "-pattern_type",
-      "glob",
-      "-i",
-      `${time}/*.jpg`,
-      "-vf",
-      "scale=720:-1",
-      "-loop",
-      "0",
-      `${time}/${outputName}`
-    );
-    const webp = ffmpeg.FS("readFile", `${time}/${outputName}`);
-    done(webp);
+  });
+
+  socket.on("webp", (done) => {
+    if (ffmpeg.isLoaded()) {
+      runWebp(ffmpeg);
+    } else {
+      queue = true;
+    }
   });
 });
+
+async function runWebp(ffmpeg) {
+  const time = Date.now().toString();
+  const rand = getRandomInt(100, 1000);
+  const outputName = "a.webp";
+  const downloadPromises = [];
+
+  ffmpeg.FS("mkdir", time);
+  for (let i = 0; i < 18; i++) {
+    let name = `${(rand + i).toString().padStart(5, "0")}.jpg`;
+
+    downloadPromises.push(
+      new Promise((resolve) => {
+        axios(encodeURI(`https://d2wwh0934dzo2k.cloudfront.net/ghibli/07 붉은 돼지 (1992)/${name}`), {
+          responseType: "arraybuffer",
+        }).then((response) => {
+          ffmpeg.FS("writeFile", `${time}/${name}`, response.data);
+          resolve();
+        });
+      })
+    );
+  }
+  await Promise.all(downloadPromises);
+  await ffmpeg.run(
+    "-framerate",
+    "12",
+    "-pattern_type",
+    "glob",
+    "-i",
+    `${time}/*.jpg`,
+    "-vf",
+    "scale=720:-1",
+    "-loop",
+    "0",
+    `${time}/${outputName}`
+  );
+  const webp = ffmpeg.FS("readFile", `${time}/${outputName}`);
+}
 
 function getRandomInt(minInclude, maxExclude) {
   return Math.floor(Math.random() * (maxExclude - minInclude)) + minInclude;
